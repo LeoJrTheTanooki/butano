b13b0c07c06f6e9faaf9b9e22faf85776e7da290
LeoJrTheTanooki <127908217+LeoJrTheTanooki@users.noreply.github.com>
2024-06-30 11:37:54 -0700

    Initialized Personal Project

--- examples/map_collision/src/main.cpp
+++ examples/map_collision/src/main.cpp
@@ -8,8 +8,10 @@
 #include "bn_regular_bg_ptr.h"
 #include "bn_sprite_text_generator.h"
 #include "bn_regular_bg_map_cell_info.h"
+#include "bn_log.h"
 
-#include "bn_sprite_items_dog.h"
+#include "bn_sprite_items_ninja.h"
+#include "bn_sprite_items_ninja_npc.h"
 #include "bn_regular_bg_items_map.h"
 
 #include "common_info.h"
@@ -20,55 +22,101 @@ int main()
     bn::core::init();
 
     constexpr bn::string_view info_text_lines[] = {
-        "PAD: move dog"
-    };
+        "PAD: move ninja"};
 
     bn::sprite_text_generator text_generator(common::variable_8x16_sprite_font);
+    bn::vector<bn::sprite_ptr, 32> text_sprites;
     common::info info("Map collision", info_text_lines, text_generator);
 
     bn::regular_bg_ptr map_bg = bn::regular_bg_items::map.create_bg(0, 0);
-    bn::sprite_ptr dog_sprite = bn::sprite_items::dog.create_sprite(0, 0);
 
-    const bn::regular_bg_map_item& map_item = bn::regular_bg_items::map.map_item();
-    bn::regular_bg_map_cell valid_map_cell = map_item.cell(0, 0);
-    int valid_tile_index = bn::regular_bg_map_cell_info(valid_map_cell).tile_index();
-    bn::point dog_map_position(16, 16);
+    bn::sprite_ptr ninja_sprite = bn::sprite_items::ninja.create_sprite(0, 0);
+    bn::sprite_ptr ninja_npc_sprite = bn::sprite_items::ninja_npc.create_sprite(16, 16);
 
-    while(true)
+    const bn::regular_bg_map_item &map_item = bn::regular_bg_items::map.map_item();
+
+    int valid_tile_array[] = {
+        bn::regular_bg_map_cell_info(map_item.cell(0, 0)).tile_index(),
+        bn::regular_bg_map_cell_info(map_item.cell(1, 0)).tile_index(),
+        bn::regular_bg_map_cell_info(map_item.cell(2, 0)).tile_index(),
+        bn::regular_bg_map_cell_info(map_item.cell(3, 0)).tile_index(),
+    };
+    bool text_active = false;
+
+    bn::point ninja_map_position(16, 16);
+    // bn::point ninja_npc_map_position(16, 16);
+
+    while (true)
     {
-        bn::point new_dog_map_position = dog_map_position;
+        bn::point new_ninja_map_position = ninja_map_position;
 
-        if(bn::keypad::left_pressed())
+        if (bn::keypad::left_pressed())
         {
-            new_dog_map_position.set_x(new_dog_map_position.x() - 1);
-            dog_sprite.set_horizontal_flip(true);
+            new_ninja_map_position.set_x(new_ninja_map_position.x() - 1);
+            ninja_sprite.set_horizontal_flip(true);
         }
-        else if(bn::keypad::right_pressed())
+        else if (bn::keypad::right_pressed())
         {
-            new_dog_map_position.set_x(new_dog_map_position.x() + 1);
-            dog_sprite.set_horizontal_flip(false);
-        }
-
-        if(bn::keypad::up_pressed())
-        {
-            new_dog_map_position.set_y(new_dog_map_position.y() - 1);
-        }
-        else if(bn::keypad::down_pressed())
-        {
-            new_dog_map_position.set_y(new_dog_map_position.y() + 1);
+            new_ninja_map_position.set_x(new_ninja_map_position.x() + 1);
+            ninja_sprite.set_horizontal_flip(false);
         }
 
-        bn::regular_bg_map_cell dog_map_cell = map_item.cell(new_dog_map_position);
-        int dog_tile_index = bn::regular_bg_map_cell_info(dog_map_cell).tile_index();
-
-        if(dog_tile_index == valid_tile_index)
+        if (bn::keypad::up_pressed())
         {
-            dog_map_position = new_dog_map_position;
+            new_ninja_map_position.set_y(new_ninja_map_position.y() - 1);
+        }
+        else if (bn::keypad::down_pressed())
+        {
+            new_ninja_map_position.set_y(new_ninja_map_position.y() + 1);
         }
 
-        bn::fixed dog_sprite_x = (dog_map_position.x() * 8) - (map_item.dimensions().width() * 4) + 4;
-        bn::fixed dog_sprite_y = (dog_map_position.y() * 8) - (map_item.dimensions().height() * 4) + 4;
-        dog_sprite.set_position(dog_sprite_x, dog_sprite_y);
+        if (bn::keypad::a_pressed())
+        {
+            if (!text_active)
+            {
+                if (
+                    (ninja_sprite.x() - ninja_npc_sprite.x() == 0 && ninja_sprite.y() - ninja_npc_sprite.y() == -16) ||
+                    (ninja_sprite.x() - ninja_npc_sprite.x() == -16 && ninja_sprite.y() - ninja_npc_sprite.y() == 0) ||
+                    (ninja_sprite.x() - ninja_npc_sprite.x() == 0 && ninja_sprite.y() - ninja_npc_sprite.y() == 16) ||
+                    (ninja_sprite.x() - ninja_npc_sprite.x() == 16 && ninja_sprite.y() - ninja_npc_sprite.y() == 0))
+                {
+                    text_generator.generate(0, 32, "Ah, hello there", text_sprites);
+                    text_active = true;
+                }
+            }
+            else
+            {
+                text_sprites.clear();
+                text_active = false;
+            }
+        }
+
+        // if (bn::keypad::b_pressed())
+        // {
+        //     // BN_LOG(ninja_map_position.x(), " ", ninja_map_position.y());
+        //     // BN_LOG(ninja_npc_map_position.x(), " ", ninja_npc_map_position.y());
+        //     // BN_LOG("Ninja Coords X:", ninja_sprite.x(), " Y:", ninja_sprite.y());
+        //     // BN_LOG("NPC Coords X:", ninja_npc_sprite.x(), " Y:", ninja_npc_sprite.y());
+        //     BN_LOG("Ninja Coords X:", ninja_sprite.x() - ninja_npc_sprite.x(), " Y:", ninja_sprite.y() - ninja_npc_sprite.y());
+        //     // BN_LOG("NPC Coords X:", ninja_npc_sprite.x() - ninja_npc_sprite.x(), " Y:", ninja_npc_sprite.y() - ninja_npc_sprite.y());
+        //     BN_LOG(" ");
+        // }
+
+        bn::regular_bg_map_cell ninja_map_cell = map_item.cell(new_ninja_map_position);
+        int ninja_tile_index = bn::regular_bg_map_cell_info(ninja_map_cell).tile_index();
+
+        for (int i = 0; i < sizeof(valid_tile_array) / sizeof(int); i++)
+        {
+            if (ninja_tile_index == valid_tile_array[i])
+            {
+                ninja_map_position = new_ninja_map_position;
+            }
+        }
+
+        bn::fixed ninja_sprite_x = (ninja_map_position.x() * 8) - (map_item.dimensions().width() * 4);
+        bn::fixed ninja_sprite_y = (ninja_map_position.y() * 8) - (map_item.dimensions().height() * 4);
+        ninja_sprite.set_position(ninja_sprite_x, ninja_sprite_y);
+        // ninja_npc_sprite.set_position()
 
         info.update();
         bn::core::update();
